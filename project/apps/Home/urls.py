4fb00d5175797abf38ccb3641e7c38344aee4545
Daniela Soler Collao <desoler1202@gmail.com>
2023-08-01 18:26:52 -0400

    add: login y logout

--- project/apps/Home/urls.py
+++ project/apps/Home/urls.py
@@ -1,9 +1,14 @@
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
+from django.contrib.auth.views import LogoutView
 from django.urls import path
-from .views import home
+from . import views
 
 app_name = "Home"
 
 urlpatterns = [
-    path("", home, name="home"),
-
+    path("", views.home, name="home"),
+    path("login/",views.login_request,name="login"),
+    path("logout/",LogoutView.as_view(template_name="Home/logout.html"),name="logout")
 ]
+
+urlpatterns += staticfiles_urlpatterns()
--- project/apps/Home/views.py
+++ project/apps/Home/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-
+from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
+from django.contrib.auth import login, logout, authenticate
+from django.shortcuts import render
+from django.urls import reverse
+from . import forms
 # Create your views here.
 
 def home(request):
     return render(request, "Home/index.html")
+
+def login_request(request):
+    if request.method == "POST":
+        form = forms.CustomAuthenticationForm(request,request.POST)
+        if form.is_valid():
+            usuario = form.cleaned_data.get("username")
+            contraseña = form.cleaned_data.get("password")
+            user = authenticate(username=usuario, password=contraseña)
+            if user is not None:
+                login(request, user)
+                return render(request, "Home/index.html", {"mensaje":"Inicio de sesión exitoso"})
+    else:
+        form = forms.CustomAuthenticationForm()
+    return render(request,"Home/login.html",{"form":form})
