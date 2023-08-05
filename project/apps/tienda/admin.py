19e304d0c5a68ff9a6f302d71c68935c6f18af60
Daniela Soler Collao <desoler1202@gmail.com>
2023-08-04 21:41:09 -0400

    add models to app.tienda

--- project/apps/tienda/admin.py
+++ project/apps/tienda/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
+from . import models
 
-# Register your models here.
+admin.site.register(models.TipoProducto)
+
+@admin.register(models.NuevoProducto)
+class NuevoProductoAdmin(admin.ModelAdmin):
+    list_display = (
+        "nombre",
+        "categoria",
+        "cantidad",
+        "precio",
+        "fecha_actualizacion"
+    )
+    list_display_links = ("nombre",)
+    search_fields = ("nombre","categoria")
+    list_filter = ("nombre",)
+    date_hierarchy = "fecha_actualizacion"
--- project/apps/tienda/models.py
+++ project/apps/tienda/models.py
@@ -1,3 +1,28 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+class TipoProducto(models.Model):
+    nombre = models.CharField(max_length=100,verbose_name="Nombre de la Categoría",unique=True)
+
+    def __str__(self) -> str:
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Categoría de Productos"
+        verbose_name_plural = "Categorías de Productos"
+
+class NuevoProducto(models.Model):
+    nombre = models.CharField(max_length=100,verbose_name="Nombre",unique=True)
+    categoria = models.ForeignKey(TipoProducto,on_delete=models.SET_NULL, null=True, blank=True)
+    cantidad = models.PositiveIntegerField(verbose_name="Cantidad")
+    precio = models.PositiveIntegerField(verbose_name="Precio")
+    descripcion = models.TextField(max_length=1000,verbose_name="Descrición")
+    imagen = models.ImageField(verbose_name="Imagen asociada al producto",upload_to="")
+    fecha_actualizacion = models.DateField(default=timezone.now,editable=False,verbose_name="Fecha de actualización")
+
+    def __str__(self) -> str:
+        return self.nombre
+
+    class Meta:
+        verbose_name = "Nuevo Producto"
+        verbose_name_plural = "Nuevos Productos"
